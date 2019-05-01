1eb0833c2f6efa5124da6e52c95270cebfbdc81c
Fmohican <florinmohican@gmail.com>
2019-05-01 17:00:33 +0300

    Enqueue new sytle.css in header

--- header.php
+++ header.php
@@ -7,6 +7,7 @@ include('function.php');
   <head>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/style.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
   <body>
